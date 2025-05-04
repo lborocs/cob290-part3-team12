078b035bb92baf13c7ae8e7554dc7008cc20b783
ForeverGinger <a.r.heaton108957@gmail.com>
2025-05-04 12:59:37 +0100

    Added additional routes that may be needed

--- api/routes/taskRoutes.js
+++ api/routes/taskRoutes.js
@@ -14,7 +14,6 @@ Output:
 router.get("/get-user-tasks", authenticateToken, (req, res) => {
     let email = req.user.email;
 
-  
         const getUserTasksQuery = `SELECT 
           task.task_id, 
           task.description, 
@@ -49,6 +48,7 @@ Output:
 */
 router.get("/get-team-tasks", authenticateToken, (req, res) => {
     let teamId = req.body.team_id;
+    //Check if req.user.email = team leader? or use team routes for this?
 
         const getTeamTasksQuery = `SELECT 
           task.task_id, 
@@ -74,6 +74,68 @@ router.get("/get-team-tasks", authenticateToken, (req, res) => {
     });
 });
   
+/*
+Getting tasks for a user for a specific team
+Input:
+- Token
+- User id
+- Team id
+Output:
+- JSON of the users tasks for the team
+*/
+router.get("/get-user-tasks-for-team", authenticateToken, (req, res) => {
+    let email = req.user.email;
+    let teamId = req.body.team_id;
+
+        const getUserTasksForTeamQuery = `SELECT 
+          task.task_id, 
+          task.description, 
+          task.manhours,
+          task.completed,
+          task.duedate,
+           
+          FROM Tasks task
+          WHERE task.user_id = ? AND task.team_id = ? 
+          ORDER BY task.duedate DESC;`
+        ;
+  
+        connection.query(getUserTasksForTeamQuery, [email, teamId], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        if (results.length === 0) {
+            return res.status(404).json({ message: "User has no current tasks" });
+          }
+        res.json({results});
+    });
+});
+
+/*
+Update task completed status to completed - Needed if demonstrating live updates
+Input:
+- Token
+- Task id
+Output:
+- Json message of successful update
+*/
+router.get("/task-complete", authenticateToken, (req, res) => {
+    let taskId = req.body.task_id;
+
+        const updateTaskCompletionQuery = `
+        UPDATE Tasks
+        SET completed = 1
+        WHERE task_id = ?;
+        `
+        ;
+  
+        connection.query(updateTaskCompletionQuery, [taskId], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        res.json({message: "Task completion updated successfully"});
+    });
+});
+
 module.exports = router;
 
 

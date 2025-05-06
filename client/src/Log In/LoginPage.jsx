c1b58b676b3054226c6859b2c5ece4ca2f3b2bd6
Chris Kelly <chris.j.k430@gmail.com>
2025-05-06 12:56:23 +0100

    hero section complete

--- client/src/Log In/LoginPage.jsx
+++ client/src/Log In/LoginPage.jsx
@@ -9,14 +9,14 @@ const LoginPage = () => {
                 <div className="pattern"></div>
             </div>
             <div className="hero">
-                <h1 className="heroHeader">Welcome to the Future of Communication</h1>
-                <p className="heroSubHeader">Join us in revolutionizing the way we connect and collaborate.</p>
+                <h1 className="heroHeader"><span className="speechmarks">"</span>We Make Tomorrow Happen <span className="focus">Today</span><span className="speechmarks">"</span></h1>
+                <img src = "../../../assets/scribble.png"/>
+                <p>- Make It All</p>
             </div>
             <div className="loginFormWrapper"><LogInform /></div>
         </div>
     );  
 
 }
-
 
 export default LoginPage;

14522ea1bf6f8bce264f34dc1fa700089f7b624a
Sam <samuelrichardson902@gmail.com>
2025-05-07 11:07:11 +0100

    changed auth token handling

--- api/authMiddleware.js
+++ api/authMiddleware.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = req.headers["authorization"];
 
   if (!token) {
     return res.status(401).json({ error: "Access token required" });

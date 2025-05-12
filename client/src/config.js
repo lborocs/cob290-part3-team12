91e4aacb65236cde9926c89c30e22c7f7064d7f8
Willmaster04 <24551828+Willmaster04@users.noreply.github.com>
2025-05-12 08:59:14 +0100

    Updated IP to new one

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://34.89.114.96";
+const API_URL = "http://34.105.143.105/";
 // v for automatic when its on the server but ignore for now
 // const API_URL = window.location.origin;
 export default API_URL;
